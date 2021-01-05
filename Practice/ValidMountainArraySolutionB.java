7f310ac575be353ef74350214e828226eab24b9d
adshre <aditi.telang208@gmail.com>
2021-01-05 15:14:05 +0530

    Create ValidMountainArraySolutionB.java

---
+++ Practice/ValidMountainArraySolutionB.java
@@ -0,0 +1,23 @@
+class ValidMountainArraySolutionB {
+    public boolean validMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+    
+        while(start<end){
+            
+            if(arr[start+1]>arr[start])
+                start++;
+            
+            
+            else if (arr[end-1]>arr[end])
+                end--;
+            
+            else
+                break;
+            
+        }
+        
+        return start!=0 && end!=arr.length-1 && start==end;
+    
+    }
+}
